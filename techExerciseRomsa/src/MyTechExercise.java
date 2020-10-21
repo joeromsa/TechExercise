a4c55ad170387866170dc5d6b7794507a9ce96ee
joe <47951551+joeromsa@users.noreply.github.com>
2020-10-21 11:48:48 -0500

    classes to connect to database.

--- techExerciseRomsa/src/MyTechExercise.java
+++ techExerciseRomsa/src/MyTechExercise.java
@@ -27,7 +27,14 @@ public class MyTechExercise extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World!");
+		try {
+			DBConnection.getDBConnection();
+			response.getWriter().append("Hello World!");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	/**
